9cef2264fc344ab99744d3cd93e58e8faf4e15c7
yangfei9215 <1154073615@qq.com>
2017-03-10 13:55:45 +0800

    commit	0310

--- src/main/java/group/yf/controller/TestAction.java
+++ src/main/java/group/yf/controller/TestAction.java
@@ -1,11 +1,17 @@
 package group.yf.controller;
 
+import java.io.File;
+import java.io.IOException;
+
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import group.yf.service.TestService;
@@ -28,5 +34,25 @@ public class TestAction {
 		int i=testService.add();
 		log.info("action>>>>>>>>"+i);
 		return mav;
+	}
+	
+	/**
+	 * 文件上传
+	 */
+	@RequestMapping("fileUpLoad")
+	public String fileUpLoad(@RequestParam("file")MultipartFile file){
+		if(!file.isEmpty()){
+			try {
+				
+				FileUtils.copyInputStreamToFile(file.getInputStream(), new File("保存地址"));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		
+		
+		return "";
 	}
 }
--- src/main/java/group/yf/controller/VolunteerAction.java
+++ src/main/java/group/yf/controller/VolunteerAction.java
@@ -15,5 +15,8 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class VolunteerAction {
 	
+//	@ResponseBody
+//	@RequestMapping(value="volunteerResult/{province}/{science}/{score}/{volunteerArea}",produces="application/json;charset=UTF-8")
+//	public String
 	
 }
